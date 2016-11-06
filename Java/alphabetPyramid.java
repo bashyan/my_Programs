51dcf6c1144a5a2b32d32b01c4c7c502c884cc6c
bashyan <rgbine@gmail.com>
2016-11-07 03:18:27 +0530

    12_Java

--- Java/alphabetPyramid.java
+++ Java/alphabetPyramid.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
 public class alphabetPyramid
 {
 	public static void main(String[] args)
 	{
-		for(int i=0;i<8;i++)
+		Scanner ab = new Scanner(System.in);
+		System.out.println("Enter no. of rows");
+		int row = ab.nextInt();
+		for(int i=row;i>0;i--)
 		{
-			for(int j=0;j<8-i;j++)
+			for(int j=0;j<row-i;j++)
 			{
-				System.out.print(" ");
+				System.out.print("  ");
 			}
-			for(int k=0; k<i;k++)
+			for(int k=0;k<i;k++)
 			{
-				System.out.print('A');
+				char a = (char) ('A' +k);
+				System.out.print(a);
 			}
+			for(int x=i-2;x>-1;x--)
+				{
+				char b = (char) ('A' +x);
+				System.out.print(b);
+				}
 			System.out.println(" ");
 		}
 	}
